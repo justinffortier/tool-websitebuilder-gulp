b385b2950bb2a92c7c6971846c618cc0c072c1d6
Justin Fortier <justinffortier@gmail.com>
2020-05-05 08:28:40 -0700

    fix: define clean actions

--- clean.js
+++ clean.js
@@ -5,6 +5,8 @@ class Delete extends Base {
   constructor (gulp, config) {
     super(gulp, config)
     gulp.task(`clean`, () => this.clean())
+    gulp.task(`cleanScss`, () => this.cleanScss())
+    gulp.task(`cleanJs`, () => this.cleanJs())
   }
 
   clean () {
