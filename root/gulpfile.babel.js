43eb73f825d226cd2b15d7c0b2665b90968801a2
Justin Fortier <justinffortier@gmail.com>
2022-03-12 13:36:34 -0800

    fix: src path

--- root/gulpfile.babel.js
+++ root/gulpfile.babel.js
@@ -10,12 +10,12 @@ gulp.WebsiteBuilder = new WebsiteBuilder(gulp, {
   "imgDestPath": "assets",
   "cssOutputFile": "all.min.css",
   "jsOutputFile": "all.min.js",
-  "scssSrcFilePath": "assets/scss/all.scss",
-  "jsSrcFilePath": "assets/js/all.js",
-  "imgSrcPath": "assets/images",
-  "scssWatchPath": "./assets/scss/**/*",
+  "scssSrcFilePath": "src/assets/scss/all.scss",
+  "jsSrcFilePath": "src/assets/js/all.js",
+  "imgSrcPath": "src/assets/images",
+  "scssWatchPath": "./src/assets/scss/**/*",
   "jsWatchPath": "assets/js/**/*",
-  "fontWatchPath": "assets/fonts/*",
-  "imageWatchPath": "assets/images/*",
-  "fontSrc": "assets/fonts"
+  "fontWatchPath": "src/assets/fonts/*",
+  "imageWatchPath": "src/assets/images/*",
+  "fontSrc": "src/assets/fonts"
 })
